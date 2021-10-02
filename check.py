058a625f758100018a1886bf8d0bf9b919f33c3d
chengscc <44265641+xuchengcheng926@users.noreply.github.com>
2021-10-02 21:52:47 +0800

    Add files via upload

--- check.py
+++ check.py
@@ -1,13 +1,14 @@
 import requests
 import time
 
-url="http://usa1.chengs.tk:8000/check/911re"
+url="http://des.chengs.tk:14856/check/911re"
 port=5000
 t=time.localtime().tm_hour
 
 def report2server(erro_port):
+  erro_port=int(erro_port)+45891
   try:
-    res=requests.get(url, params={"erro_port":int(erro_port)})
+    res=requests.get(url, params={"erro_port":erro_port},timeout=5)
   except:
     pass
   else:
@@ -17,20 +18,22 @@ def report2server(erro_port):
 def main():
   print('')
   print("--------------------------------------------------------------")
-  if t>5 and t<14:
+  if t>50 and t<12:
     print(f"{time.localtime().tm_mon}.{time.localtime().tm_mday} {time.localtime().tm_hour}:{time.localtime().tm_min} don't change")
     print("--------------------------------------------------------------")
     exit(0)
   else:
     print(f"{time.localtime().tm_mon}.{time.localtime().tm_mday} {time.localtime().tm_hour}:{time.localtime().tm_min} can change ")
 
-    for i in range(5):
-      proxies = {"http":"socks5://usa1.chengs.tk:"+str(port+i)}
+    for i in range(10):
+      proxies = {"http":"socks5://des.chengs.tk:"+str(port+i)}
       try:
-        requests.get('http://google.com', proxies=proxies)
+        requests.get('http://google.com', proxies=proxies,timeout=10)
       except:
         print('faild'+':'+str(proxies)+", report to server: "+report2server(port+i))
       else:
         print('success'+':'+str(proxies)+", don't report")
   print("--------------------------------------------------------------")
 main()
+#requests.get(url, params={"erro_port":5004+45891})
+#requests.get("http://pushplus.hxtrip.com/send",params={"token":"d4caebf0eac54e39897337ba1679f3d9", "title":"911re port change","content":f"{port}","template":"html"})
--- serve.py
+++ serve.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 import uvicorn as uvicorn
-import os
 import requests
+import os 
+import sys
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+import json
 
 
-push_tk=""
-host_name=""
-app = FastAPI()
 
+area=["-changeproxy/FR/IDF/Paris","-changeproxy/DE/BE/Berlin","-changeproxy/GB/ENG/London"]
 error_dict={
     '5000':0,
     '5001':0,
@@ -18,25 +21,64 @@ error_dict={
     '5006':0,
     '5007':0,
     '5008':0,
-    '5009':0, 
+    '5009':0,
+    '5010':0,
+    '5011':0,
+    '5012':0,
+    '5013':0,
+    '5014':0,
+    '5015':0,
+    '5016':0,
+    '5017':0,
+    '5018':0,
+    '5019':0,
+    '5020':0,
+    '5021':0,
+    '5022':0,
+    '5023':0,
+    '5024':0,
+    '5025':0,
+    '5026':0,
+    '5027':0,
+    '5028':0,
+    '5029':0,
+    '5030':0,
+    'totol':0
 }
+
+app = FastAPI()
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=400)
 
 def portchange(port):
     error_dict[port]=error_dict[port]+1
-    if error_dict[port] > 6 :
+    if error_dict[port] > 10 :
+        error_dict['totol']=error_dict['totol']+1
         error_dict[port]=0
         os.system(f"C:\\Users\\Administrator\\Desktop\\create.bat {port}")
-        request.get("http://pushplus.hxtrip.com/send", params={"token":push_tk,"title":"911re port change","content":f"{port}","template":"html"})
+        try:
+            print("")
+            requests.get("http://pushplus.hxtrip.com/send",params={"token":"d4caebf0eac54e39897337ba1679f3d9", "title":"911re port change","content":f"{port}","template":"html"})
+        except:
+            pass
+        
+    print(str(port))
     print(error_dict)
-    
 
 @app.get("/check/911re")
 async def read_root(erro_port):
-    print(int(erro_port))
+    erro_port=str(int(erro_port)-45891)
     portchange(erro_port)
-    return {"message": "Hello World"}
+   
 
 
 
 if __name__ == '__main__':
-    uvicorn.run(app='serve:app', host=host_name, port=8000, reload = True)
+    uvicorn.run(app='serve:app', host="des.chengs.tk", port=14856, reload = True)
+
